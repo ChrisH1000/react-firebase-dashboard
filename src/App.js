e61b12238933a94d763c34111ed708acefe63acf
Chris <ChrisH1000@icloud.com>
2023-05-05 14:30:21 +0800

    setup admin route

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './pages/Signup';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import Users from './pages/Users';
 import { UserProvider } from './firebase/UserProvider';
 import Profile from './pages/Profile';
 import ProfileRedirect from './router/ProfileRedirect';
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import PrivateRoute from './router/PrivateRoute';
+import AdminRoute from './router/AdminRoute';
 
 function App() {
   return (
@@ -23,6 +25,7 @@ function App() {
               <ProfileRedirect exact path="/signup" component={Signup} />
               <PrivateRoute exact path="/profile/:id" component={Profile} />
               <ProfileRedirect exact path="/login" component={Login} />
+              <AdminRoute exact path="/users" component={Users} />
               <Route exact path="/">
                 <Redirect to="/login" />
               </Route>
